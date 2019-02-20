51a4ef21fdc708b7b6987798b92884854d1de9ae
nfgoto <gotoflorian.pro@gmail.com>
2019-02-20 22:10:34 +0100

    Updates

---
+++ precache-manifest.2c303ef41941b38e365783df2e24ba64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8892a90bc6fc42e9807e",
+    "url": "/go2infinite/static/css/main.b82a0218.chunk.css"
+  },
+  {
+    "revision": "8892a90bc6fc42e9807e",
+    "url": "/go2infinite/static/js/main.8892a90b.chunk.js"
+  },
+  {
+    "revision": "febacf13b61e9130890e",
+    "url": "/go2infinite/static/js/runtime~main.febacf13.js"
+  },
+  {
+    "revision": "49d01db24faf533fdf6f",
+    "url": "/go2infinite/static/js/2.49d01db2.chunk.js"
+  },
+  {
+    "revision": "7fa0bdab2b28a71347f2f4a04ff28b2d",
+    "url": "/go2infinite/index.html"
+  }
+];
